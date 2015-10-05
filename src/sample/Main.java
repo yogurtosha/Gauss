ac139e599cf26e859f38762daca698ec604e049a
yogurtosha <yogurtosha2.0@gmail.com>
2015-10-06 00:39:23 +0300

    gauss in process :dancer:

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,10 +10,24 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+      /* Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        primaryStage.show();*/
+
+        double[][] a = {
+                {4, -3, 2, -1, 8},
+                {3, -2, 1, -3, 7},
+                {5, -3, 1, -8, 1}
+        };
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i][0] + "x+" + a[i][1] + "y+" + a[i][2] + "b+" + a[i][3] + "z = " + a[i][4]);
+        }
+        System.out.println();
+
+        Gauss gauss = new Gauss(a);
+        gauss.calculateMatrix();
+
     }
 
 
